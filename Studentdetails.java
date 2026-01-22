6b8950abcd624174141127ae27fe7cd2d11b6c1f
IXsushanthreddy <saisushanth.reddy@innovatechs.com>
2026-01-22 15:03:23 +0530

    Update Studentdetails.java

--- Studentdetails.java
+++ Studentdetails.java
@@ -2,37 +2,30 @@
 import java.util.Scanner;
 
 class Student{
-    //creating private variables
     private int id;
     private String name;
     private int score;
-    //getter method for id
     public int getId(){
         return id;
     }
-    //getter method for name
     public String getName(){
         return name;
     }
-    //getter method for score
     public int getScore(){
         return score;
     }
 
-    //setter method for all id,name,score
     public void setDetails(int id,String name,int score){
         this.id=id;
         this.name=name;
         this.score=score;
     }
-    //displays the student details
     void displayDetails(int id,String name,int score){
         setDetails(id,name,score);
         System.out.println("Student ID: "+getId());
         System.out.println("Student Name: "+getName());
         System.out.println("Student Score: "+getScore());
     }
-    //checks student is passed if passed returns true
     boolean isPassed(){
         if(score>=40){
             return true;
