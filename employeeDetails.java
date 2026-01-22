7434ca9bf8be5f5323bcc6a686f7b06d42756b03
IXsushanthreddy <saisushanth.reddy@innovatechs.com>
2026-01-22 12:59:26 +0530

    all programs

---
+++ employeeDetails.java
@@ -0,0 +1,38 @@
+class Employee{
+    int id;
+    String name;
+    double monthlySalary;
+    void info(int id,String name,double  monthlySalary){
+        this.id=id;
+        this.name=name;
+        this.monthlySalary=monthlySalary;
+    }
+    void calculateAnnualSalary(){
+        double annualSalary=(12*monthlySalary);
+        System.out.println("the annual salary:"+annualSalary);
+
+    }
+    void displayEmployeeDetails(){
+        System.out.println("Student ID: "+id);
+        System.out.println("Student Name: "+name);
+        System.out.println("Student Score: "+monthlySalary);
+    }
+}
+public class employeeDetails{
+    public static void main(String[] args) {
+        Employee emp1=new Employee();   
+        Employee emp2=new Employee();
+        emp1.info(101,"sushanth",100000);
+        emp2.info(102,"Reddy",120000);
+        
+        emp1.displayEmployeeDetails();
+        emp1.calculateAnnualSalary();
+        
+        emp2.displayEmployeeDetails();
+        emp2.calculateAnnualSalary();
+
+
+        
+    }
+}
+
